8da6d379de2fa12e401ed4d902002205ed65c295
baocongchen <peter_tran_cm@yahoo.com>
2022-07-11 17:35:29 -0400

    added verifier

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,20 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.9.0;
+import "./verifier.sol";
+import "./ERC721Mintable.sol";
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+contract SquareVerifier is Verifier {}
 
-// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is CustomERC721Token {
+    struct solutions {
+        uint256 index;
+        address addressSol;
+        uint256 solutionProvided;
+    }
 
-// TODO define a solutions struct that can hold an index & an address
+    solutions[] solutionsArray;
 
-// TODO define an array of the above struct
+    SquareVerifier public squareVerifier;
 
-// TODO define a mapping to store unique solutions submitted
+    // mapping that stores unique solutions submitted
+    mapping(bytes32 => solutions) uniqueSolutions;
 
-// TODO Create an event to emit when a solution is added
+    // event to emit when a solution is added
+    event solutionAdded(address from, uint256 index1);
 
-// TODO Create a function to add the solutions to the array and emit the event
+    constructor(address verifier) public {
+        squareVerifier = SquareVerifier(verifier);
+    }
 
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
+    function addSolution(
+        uint256 _index,
+        address _address,
+        uint256[2] memory A,
+        uint256[2][2] memory B,
+        uint256[2] memory C,
+        uint256[2] memory INPUT
+    ) public {
+        solutions memory Solution = solutions({
+            index: _index,
+            addressSol: _address,
+            solutionProvided: 1
+        });
+        bytes32 uniqueKey = keccak256(abi.encodePacked(A, B, C, INPUT));
+        uniqueSolutions[uniqueKey] = Solution;
+        solutionsArray.push(Solution);
+        emit solutionAdded(address1, index1);
+    }
+
+    function getSolutionsCount() external view returns (uint256) {
+        return solutionsArray.length;
+    }
+
+    // function to mint new NFT when solution has been verified
+    function mint(
+        uint256[2] memory A,
+        uint256[2][2] memory B,
+        uint256[2] memory C,
+        uint256[2] memory INPUT,
+        uint256 ID
+    ) public returns (bool) {
+        bytes32 uniqueKeyCheck = keccak256(abi.encodePacked(A, B, C, INPUT));
+        if (uniqueSolutions[uniqueKeyCheck].solutionProvided == 1) {
+            revert("Solution not unique! Must provide a new solution");
+        }
+        if (!squareVerifier.verifyTx(A, B, C, INPUT)) {
+            revert("Solution wrong! Must provide a correct solution");
+        }
+        addSolution(ID, msg.sender, A, B, C, INPUT);
+        bool success = mint(msg.sender, ID);
+        return success;
+    }
+}
